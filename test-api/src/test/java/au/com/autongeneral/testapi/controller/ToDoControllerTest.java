f152b82d8e2bfe5f7b3df9bd9c11e0bd5ce7a89a
unknown <meghabbabar@gmail.com>
2019-07-30 13:54:46 +1000

    Initial Commit

--- test-api/src/test/java/au/com/autongeneral/testapi/controller/ToDoControllerTest.java
+++ test-api/src/test/java/au/com/autongeneral/testapi/controller/ToDoControllerTest.java
@@ -1,37 +1,64 @@
 package au.com.autongeneral.testapi.controller;
 
-import org.easymock.EasyMockSupport;
-import org.easymock.Mock;
-import org.easymock.TestSubject;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import java.util.Optional;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import au.com.autongeneral.testapi.entities.ToDoItem;
+import au.com.autongeneral.testapi.model.ToDoItemAddRequest;
 import au.com.autongeneral.testapi.repository.TodoRepository;
 import au.com.autongeneral.testapi.validator.InputValidator;
 
 @RunWith(MockitoJUnitRunner.class)
-public class ToDoControllerTest extends EasyMockSupport {
+public class ToDoControllerTest {
 
 	@Mock
-	private TodoRepository repository;
+	TodoRepository repository;
 
 	@Mock
-	private InputValidator inputValidator;
-
-	@TestSubject
-	private ToDoController toDoController = new ToDoController(repository, inputValidator);
+	private InputValidator inputValidator = new InputValidator();
+	
+	@Mock
+	private ToDoController controller;
+	
+	@Before
+    public void setup(){
+        // init mocks
+		controller = new ToDoController(repository, inputValidator);
+    }
 
 	@Test
 	public void shouldDelegateToRepositoryToAddTodo() {
-		
+		ToDoItemAddRequest request = new ToDoItemAddRequest("First");
+		ToDoItem item = new ToDoItem("First");
+		Mockito.when(repository.saveAndFlush(item)).thenReturn(item);
+		ResponseEntity<ToDoItem> response = controller.createTodoItem(request);
+		assertNotNull(response);
+		assertThat(response.getStatusCode(), is(HttpStatus.OK));
+		ToDoItem actual = repository.saveAndFlush(item);
+		assertThat(actual, is(item));
 
 	}
 
 	@Test
 	public void shouldDelegateToRepositoryToGetTodo() {
 
-		
+		ToDoItem item = new ToDoItem("First");
+		Mockito.when(repository.findById(1l)).thenReturn(Optional.of(item));
+		ResponseEntity<ToDoItem> response = controller.retriveToDoTask(1l);
+		assertNotNull(response);
+		assertThat(response.getStatusCode(), is(HttpStatus.OK));
 
 	}
 
